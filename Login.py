cc92119368e2e433459783c73dd4681a1fd1bd8a
nwjn <127889040+nwjn@users.noreply.github.com>
2023-03-15 23:10:03 -0700

    timeout

--- Login.py
+++ Login.py
@@ -1,14 +1,82 @@
-from os import system
+import os
+import re
+import time
+import threading
+import signal
+import sys
 
-while True:
-  correct_password = "q"
-  u = input("Please enter your email: ")
-  if "@" not in u:
-    print("Invalid email. Try again.")
+PATTERN = re.compile(r"^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$")
+CORRECT_PASSWORD = "q"
+
+def match(PATTERN: re.Pattern, input: str) -> bool:
+  if re.fullmatch(string=input, pattern=PATTERN):
+    return True
   else:
+    return False
+
+      
+from threading import Thread
+import functools
+
+def timeout(seconds_before_timeout):
+    def deco(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            res = [Exception('function [%s] timeout [%s seconds] exceeded!' % (func.__name__, seconds_before_timeout))]
+            def newFunc():
+                try:
+                    res[0] = func(*args, **kwargs)
+                except Exception as e:
+                    res[0] = e
+            t = Thread(target=newFunc)
+            t.daemon = True
+            try:
+                t.start()
+                t.join(seconds_before_timeout)
+            except Exception as e:
+                print('error starting thread')
+                raise e
+            ret = res[0]
+            if isinstance(ret, BaseException):
+                raise ret
+            return ret
+        return wrapper
+    return deco
+
+@timeout(60)
+def get_password(email: str):
+  while True:
     i = input("Please enter your password: ")
-    if correct_password == i:
+    os.system("cls")
+    print("Email: ", email)
+
+    if CORRECT_PASSWORD == i:
       print("Login correct!")
       break
     else:
       print("Login incorrect.")
+
+def main():
+  
+  os.system("cls")
+  
+  while True:
+    u = input("Please enter your email: ")
+    os.system("cls")
+    
+    if match(PATTERN, u):
+      break
+
+    print("Invalid email.")
+    
+  print("Email: ", u)
+  
+  try:
+    get_password(u)
+  except:
+    os.system("cls")
+    sys.exit("Timeout!")
+      
+  print("reached")
+      
+main()
--- Process.py
+++ Process.py
@@ -1,16 +1,24 @@
 import subprocess
+import sys
+from os import system
 
 # p = subprocess.Popen(["dir"], shell=True)
 # p = subprocess.check_output("Subprocess.py", shell=True)
 p = subprocess.check_output(["echo", "Hello world!"], shell=True)
 print(p)
+# system("cls")
 
-
-process = subprocess.Popen(["ping", "192.168.7.229"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-try:
-  outs, errs = process.communicate(timeout=5)
-  print("outs: ", outs.decode('ascii').strip("\r\n"))
-  print("errs: ", errs)
-except subprocess.TimeoutExpired:
-  process.kill()
-  print("process failed")
+# process = subprocess.Popen(["ping", "10.20.30.173"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+for n in range(ord('a'), ord('z') + 1):
+  process = subprocess.Popen([sys.executable, "Login.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+  try:
+    outs, errs = process.communicate(input=b"q",timeout=5)
+  except subprocess.TimeoutExpired:
+    process.kill()
+    print("process failed")
+    break
+  str_outs  = outs.decode('ascii').strip("\r\n")
+  print("outs: ", str_outs)
+  if "incorrect" not in str_outs:
+    print("The password is %c!", chr(n))
+    break
